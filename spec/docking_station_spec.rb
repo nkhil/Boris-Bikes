2a1730cbfd981bbbd96ad75d8008d9b91954e50a
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-08 16:50:09 +0000

    Added test to get new bike instance and to check if it is working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,4 +9,13 @@ describe DockingStation do
 
   it { should respond_to 'release_bike' }
 
+  it "Should get a bike" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to be_a Bike
+  end
+
+  it "Should be a working bike" do
+    bike = DockingStation.new.release_bike
+    expect(bike.working?).to eq(true)
+  end
 end
